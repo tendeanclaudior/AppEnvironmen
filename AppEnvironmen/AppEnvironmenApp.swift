84d2f4aa850df458fbce80fc734bb64ed030820d
Dio Tendean <tendeanclaudio1234@gmail.com>
2023-05-16 15:54:00 +0800

    Initial Commit

---
+++ AppEnvironmen/AppEnvironmenApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppEnvironmenApp.swift
+//  AppEnvironmen
+//
+//  Created by Claudio Tendean on 16/05/23.
+//
+
+import SwiftUI
+
+@main
+struct AppEnvironmenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
