2d6e2bbc3e848ad9557d65580d9042ef8ba78170
Dio Tendean <tendeanclaudio1234@gmail.com>
2023-05-22 09:13:09 +0800

    Initial Commit

---
+++ GeometryReader/GeometryReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryReaderApp.swift
+//  GeometryReader
+//
+//  Created by Claudio Tendean on 22/05/23.
+//
+
+import SwiftUI
+
+@main
+struct GeometryReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
