e18f00abfd4995866327e81804056b5e789625fe
Dio Tendean <tendeanclaudio1234@gmail.com>
2023-05-22 13:19:55 +0800

    first commit

--- GeometryReader/ContentView.swift
+++ GeometryReader/ContentView.swift
@@ -9,18 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView {
+            VStack {
+                Kolom(gambar1: "g1", gambar2: "g2", gambar3: "g3")
+                Kolom(gambar1: "g3", gambar2: "g4", gambar3: "g5")
+                Text("My Gallery")
+            }
+            .navigationBarTitle("Galeri")
+        }.navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Kolom : View {
+    var gambar1, gambar2, gambar3 : String
+    var body: some View {
+        GeometryReader {
+            geo in
+            HStack {
+                Image(self.gambar1)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: geo.size.width/3, height: geo.size.height)
+                    .clipped()
+                
+                Image(self.gambar1)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: geo.size.width/3, height: geo.size.height)
+                    .clipped()
+                
+                Image(self.gambar1)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: geo.size.width/3, height: geo.size.height)
+                    .clipped()
+            }
+        }
     }
 }
